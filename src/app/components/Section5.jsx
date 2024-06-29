2d1fc737082a74f39833dd7cc53cc30030792ed2
Ammar Farooq <101990063+mr-ammar-1@users.noreply.github.com>
2024-06-29 20:48:09 +0500

    Create Section5.jsx

---
+++ src/app/components/Section5.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Section5 = () => {
+  return (
+    <div className='mx-auto mt-16 flex lg:flex-nowrap flex-wrap items-center justify-center border-white border-1 shadow-lg w-[86%] bg-gradient-to-l from-blue-100 to-white '>
+   
+    <div className='mr-10 lg:p-0 p-10'>
+    <h1 className='text-left w-full lg:text-4xl text-3xl font-semibold '><span className='text-blue-600'> APP </span> INTEGRATION</h1>
+    <p className='text-left mx-auto xl:w-[627px] w-full mt-10 text-2xl font-normal text-gray-500'>eamlessly connects your custom mobile apps to critical backend systems, services and data sources. We integrate iOS, Android and hybrid apps with any REST or SOAP based API, database, ERP like SAP, CRM like Salesforce, or other business system via robust, scalable, and highly secure APIs. Following best practices for API design, our team handles your end-to-end integration project from planning and architecture </p>
+    </div>   
+    <img className='mt-10 mb-10 ' src="./App Integration.png" alt="" />
+  </div>
+  )
+}
+
+export default Section5
