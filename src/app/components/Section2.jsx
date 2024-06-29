3901f16f54663c277ab70e1833cac068b3bbaf7b
Ammar Farooq <101990063+mr-ammar-1@users.noreply.github.com>
2024-06-29 20:47:14 +0500

    Update Section2.jsx

--- src/app/components/Section2.jsx
+++ src/app/components/Section2.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Section2 = () => {
   return (
-    <div className='lg:ml-36 ml-5 flex items-center justify-center border-white border-1 shadow-xl w-[1200px] bg-gradient-to-r from-blue-100 to-white '>
+    <div className='mx-auto overflow-hidden flex lg:flex-nowrap flex-wrap items-center justify-center border-white border-1 shadow-lg w-[86%] bg-gradient-to-r from-blue-100 to-white '>
       
-      <img src="./Mobile Holding.png" alt="" />
-      <div>
-      <h1 className='text-left w-full lg:text-4xl text-3xl font-semibold '><span className='text-blue-600'> IOS </span> App DEVELOPMENT</h1>
-      <p className='text-left mx-auto xl:w-[630px] w-full mt-10 text-2xl font-medium text-gray-500'>OptimaGeeks is a leading iOS app development company that specializes in developing native iOS apps for the iPhone and iPad using Swift, Objective-C, and modern Apple frameworks like SwiftUI. Our skilled iOS developers build high-performing, user-friendly iOS apps with stunning UI/UX seamlessly optimized for all Apple devices. Using agile development, we continuously enhance your custom iOS app post-</p>
+      <img className='lg:ml-[-105px]' src="./Mobile Holding.png" alt="" />
+      <div className='lg:mt-0 mt-10 lg:p-0 p-10' >
+      <h1 className='text-left w-full lg:text-4xl text-2xl font-semibold '><span className='text-blue-600'> IOS </span> APP DEVELOPMENT</h1>
+      <p className='text-left mx-auto xl:w-[630px] w-full mt-10 lg:text-2xl text-xl mr-2 font-normal text-gray-500'>OptimaGeeks is a leading iOS app development company that specializes in developing native iOS apps for the iPhone and iPad using Swift, Objective-C, and modern Apple frameworks like SwiftUI. Our skilled iOS developers build high-performing, user-friendly iOS apps with stunning UI/UX seamlessly optimized for all Apple devices. Using agile development, we continuously enhance your custom iOS app post-</p>
       </div>    
     </div>
   )
