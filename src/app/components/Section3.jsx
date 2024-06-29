f7d99a11a9bc0476ab87f371957899dd22377009
Ammar Farooq <101990063+mr-ammar-1@users.noreply.github.com>
2024-06-29 20:47:44 +0500

    Create Section3.jsx

---
+++ src/app/components/Section3.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Section3 = () => {
+  return (
+    <div className='mx-auto mt-16 flex lg:flex-nowrap flex-wrap items-center justify-center border-white border-1 shadow-lg w-[86%] bg-gradient-to-l from-blue-100 to-white '>
+   
+      <div className='lg:p-0 p-10'>
+      <h1 className='text-left w-full lg:text-4xl text-3xl font-semibold '><span className='text-blue-600'> Andriod </span> APP DEVELOPMENT</h1>
+      <p className='text-left mx-auto xl:w-[640px] w-full mt-10 text-2xl font-normal text-gray-500'>With multiple Google-certified Android developers on staff, OptimaGeeks employs Kotlin, Java, and the latest Android Studio framework to build secure, scalable native Android apps with delightful user experiences tailored for Android phones, TV, and wearables. Leveraging Firebase, Jetpack libraries, and popular technologies like Flutter, we create the highest quality Android apps that users love</p>
+      </div>   
+      <img className='mt-10 lg:ml-[105px]' src="./Andriod App Development.png" alt="" />
+    </div>
+  )
+}
+
+export default Section3
