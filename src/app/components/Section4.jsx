3b893b5714518e96260f26187eedb419c8bad295
Ammar Farooq <101990063+mr-ammar-1@users.noreply.github.com>
2024-06-29 20:47:54 +0500

    Create Section4.jsx

---
+++ src/app/components/Section4.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Section4 = () => {
+  return (
+    <div className='mx-auto mt-16 overflow-hidden flex lg:flex-nowrap gap-6 flex-wrap items-center justify-center border-white border-1 shadow-lg w-[86%] bg-gradient-to-r from-blue-100 to-white '>
+      
+    <img className='mt-10 mb-10 lg:ml-[-100px]' src="./Custom Mobile App.png" alt="" />
+    <div className='lg:p-0 p-10' >
+    <h1 className='text-left w-full lg:text-4xl text-3xl font-semibold '><span className='text-blue-600'> Custom </span>Mobile App Development</h1>
+    <p className='text-left mx-auto xl:w-[620px] w-full mt-10 text-2xl mr-2 font-normal text-gray-500'>OptimaGeeks is a leading iOS app development company that specializes in developing native iOS apps for the iPhone and iPad using Swift, Objective-C, and modern Apple frameworks like SwiftUI. Our skilled iOS developers build high-performing, user-friendly iOS apps with stunning UI/UX seamlessly optimized for all Apple devices. Using agile development, we continuously enhance your custom iOS app post-</p>
+    </div>    
+  </div>
+  )
+}
+
+export default Section4
