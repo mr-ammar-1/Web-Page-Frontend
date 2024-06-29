9663e5754a78eaa85ee40ceb9314ed5a7896337d
Ammar Farooq <101990063+mr-ammar-1@users.noreply.github.com>
2024-06-29 20:48:20 +0500

    Create Section6.jsx

---
+++ src/app/components/Section6.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Section6 = () => {
+  return (
+    <div className='mx-auto mb-16 mt-16 flex lg:flex-nowrap flex-wrap gap-4 items-center justify-center border-white border-1 shadow-lg w-[86%] bg-gradient-to-r from-blue-100 to-white '>
+      
+    <img className='mt-10 mb-10' src="./Quality Assurance.png" alt="" />
+    <div className='lg:mt-0 mt-10 lg:p-0 p-10' >
+    <h1 className='text-left w-full lg:text-4xl text-3xl font-semibold '><span className='text-blue-600'> TESTING </span>& QUALITY ASSURANCE </h1>
+    <p className='text-left mx-auto xl:w-[630px] w-full mt-10 text-2xl mr-2 font-normal text-gray-500'>Our rigorous testing process includes automated testing for coverage and manual real-world testing for precision. We test all functionalities, UI flows, integrations, security, and edge cases to ensure rock-solid quality and flawless functionality at launch. Using advanced tools for test automation, bug tracking, and test reporting, we provide complete transparency into testing progress. </p>
+    </div>    
+  </div>
+  )
+}
+
+export default Section6
