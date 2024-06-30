05a8ef9335e2944dcfd83c3b3352cfc6794a62bb
Ammar Farooq <101990063+mr-ammar-1@users.noreply.github.com>
2024-06-30 11:00:36 +0500

    Configuration of Images

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,13 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Section1 from "./components/Section1";
 import Section2 from "./components/Section2";
+import Section3 from "./components/Section3";
+import Section4 from "./components/Section4";
+import Section5 from "./components/Section5";
+import Section6 from "./components/Section6";
+import MADServiceProcess from "./components/MADServiceProcess";
+import TechandFram from "./components/TechandFram";
+import OurLatestProjects from "./components/OurLatestProjects";
 
 export default function Home() {
   return (
@@ -12,6 +19,13 @@ export default function Home() {
    <Header/>
    <Section1/>
    <Section2/>
+   <Section3/>
+   <Section4/>
+   <Section5/>
+   <Section6/>
+   <MADServiceProcess/>
+   <TechandFram/>
+   <OurLatestProjects/>
 
   </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: { 
+        "primary": "#1E1E1E",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
