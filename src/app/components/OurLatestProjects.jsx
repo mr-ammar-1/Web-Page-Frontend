7e61c4090cd5dc90162edb64638737cd5f36fcc8
Ammar Farooq <101990063+mr-ammar-1@users.noreply.github.com>
2024-06-30 13:44:49 +0500

    Update OurLatestProjects.jsx

--- src/app/components/OurLatestProjects.jsx
+++ src/app/components/OurLatestProjects.jsx
@@ -4,7 +4,7 @@ import Slider from './Slider'
 const OurLatestProjects = () => {
   return (
     <div className='flex flex-wrap mx-auto w-[86%] mb-16'>
-        <div className='flex-col my-16'>
+        <div className='flex-col mt-16 mb-16'>
         <h1 className='text-Left w-full lg:text-4xl text-3xl font-bold'>Our Latest Projects </h1>
 
 <p className='text-left xl:w-[420px] w-full mt-10 text-xl mr-30 font-normal text-gray-500'>We’ve been helping customers since 2009 and take pride in delivering high-quality custom services designed to help you build, grow, and revolutionize your industry.</p>
