71b0965a17f9318a1b25258dc56bdfecad6584da
Ammar Farooq <101990063+mr-ammar-1@users.noreply.github.com>
2024-07-01 13:05:45 +0500

    Update ReadyToStart.jsx
    Responsiveness

--- src/app/components/ReadyToStart.jsx
+++ src/app/components/ReadyToStart.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
 const ReadyToStart = () => {
+    
+    {/* <img className='w-full' src="./KeyboardBackground.png" alt="" /> */}
   return (
-    <div className='mt-20 relative' >
-      <img className='w-full' src="./KeyboardBackground.png" alt="" />
-      <div className='absolute mx-auto inset-20 text-center w-[86%]'>
-      <h1 className='text-center w-full lg:text-4xl text-white text-3xl font-semibold'>Ready To Get started ?</h1>
-    <p className='text-center mx-auto xl:w-[890px] text-customWhite w-full mt-10 text-2xl mr-30 font-normal'>Achieve your business goals with our successful Methodology, renowned for its structured planning and consistent results.</p>
-    <button className='text-white bg-transparent border-2 font-normal text-xl mt-16 px-16 py-3 rounded-lg'>Let's Talk</button>
-      </div>
+    <div className="mt-20 bg-[url('/KeyboardBackground.png')] py-20 flex justify-center" >
+    <div className='w-[95%] md:w-[70%] lg:w-[65%] text-center'>
+      <h1 className='text-white text-4xl font-semibold mb-5'>Ready To Get started ?</h1>
+    <p className='text-customWhite text-2xl font-normal'>Achieve your business goals with our successful Methodology, renowned for its structured planning and consistent results.</p>
+    <button className='text-white bg-transparent border-2 font-normal text-xl px-16 py-3 rounded-lg mt-10 hover:bg-white hover:text-primary transition-all duration-300'>Let's Talk</button>
+      </div>  
     </div>
   )
 }
